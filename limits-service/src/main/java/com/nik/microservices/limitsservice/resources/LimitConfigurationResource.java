26537db502fb166d7b3b9265eb974cfcb293d9f4
dnikhil135 <dnikhil11@gmail.com>
2018-04-28 11:56:09 +0530

    MY First Commit

---
+++ limits-service/src/main/java/com/nik/microservices/limitsservice/resources/LimitConfigurationResource.java
@@ -0,0 +1,20 @@
+package com.nik.microservices.limitsservice.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nik.microservices.limitsservice.model.Cofiguration;
+import com.nik.microservices.limitsservice.model.LimitConfiguration;
+
+@RestController
+public class LimitConfigurationResource {
+
+	@Autowired
+	Cofiguration cofiguration;
+	
+	@GetMapping("/limitConfig")
+	public LimitConfiguration retriveLimitConfiguration() {
+		return new LimitConfiguration(cofiguration.getMaximum(),cofiguration.getMinimum());
+	}
+}
