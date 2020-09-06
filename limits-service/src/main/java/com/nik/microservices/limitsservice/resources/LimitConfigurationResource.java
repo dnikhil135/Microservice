a6e3558d960c372bacbc928adc3a36f2aff8998e
dnikhil135 <dnikhil11@gmail.com>
2020-09-05 20:08:08 -0600

    added comment

--- limits-service/src/main/java/com/nik/microservices/limitsservice/resources/LimitConfigurationResource.java
+++ limits-service/src/main/java/com/nik/microservices/limitsservice/resources/LimitConfigurationResource.java
@@ -14,6 +14,7 @@ public class LimitConfigurationResource {
 	Cofiguration cofiguration;
 	
 	@GetMapping("/limitConfig")
+	//Testing
 	public LimitConfiguration retriveLimitConfiguration() {
 		return new LimitConfiguration(cofiguration.getMaximum(),cofiguration.getMinimum());
 	}
